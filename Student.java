7678d46d8a71ee2d19d45b98e6152c484ee68316
Jsplix <jkjin112@gmail.com>
2024-04-06 23:59:23 +0900

    Main, Student class 생성

---
+++ Student.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Student {
+    private ArrayList<Student> studentsList = new ArrayList<Student>();
+    private String studentId;
+    private String name;
+    private int korScore;
+    private int engScore;
+    private int mathScore;
+
+
+}
