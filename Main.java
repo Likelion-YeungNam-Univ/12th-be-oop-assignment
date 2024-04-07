ee8484608e7d4b6f29ca91f7c52f617f37e9a444
icegosimperson <hooho931@gmail.com>
2024-04-07 15:40:52 +0900

    Student class 구현

--- Main.java
+++ Main.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    // 학생 정보를 저장할 ArrayList 생성
+    // private static ArrayList<Student> students = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("학생 관리 프로그램입니다. ");
+        System.out.println("1번 : 학생 등록, 2번 : 전체 조회, 3번 : 학생 검색, 4번 : 학생 정보 수정, 5번 : 학생 삭제");
+
+        int input = sc.nextInt();
+        switch (input) {
+            case 1:
+                System.out.println("1번: 학생 등록 기능입니다.");
+                break;
+            case 2:
+                System.out.println("2번: 전체 조회 기능입니다.");
+                break;
+            case 3:
+                System.out.println("3번: 학생 검색 기능입니다.");
+                break;
+            case 4:
+                System.out.println("4번: 학생 정보 수정 기능입니다.");
+                break;
+            case 5:
+                System.out.println("5번: 학생 삭제 기능입니다.");
+                break;
+        }
+    }
 }
+
+
--- Student.java
+++ Student.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private String studentId;
+    private String name;
+    private int koreanScore;
+    private int englishScore;
+    private int mathScore;
+
+    // 생성자 추가
+    public Student(String studentId, String name, int koreanScore, int englishScore, int mathScore) {
+        this.studentId = studentId;
+        this.name = name;
+        this.koreanScore = koreanScore;
+        this.englishScore = englishScore;
+        this.mathScore = mathScore;
+    }
+    // Getter and setter methods
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getKoreanScore() {
+        return koreanScore;
+    }
+
+    public void setKoreanScore(int koreanScore) {
+        this.koreanScore = koreanScore;
+    }
+
+    public int getEnglishScore() {
+        return englishScore;
+    }
+
+    public void setEnglishScore(int englishScore) {
+        this.englishScore = englishScore;
+    }
+
+    public int getMathScore() {
+        return mathScore;
+    }
+
+    public void setMathScore(int mathScore) {
+        this.mathScore = mathScore;
+    }
+
+    // toString method to display student information
+    @Override
+    public String toString() {
+        return "학번: " + studentId + ", 이름: " + name + ", 국어 성적: " + koreanScore + ", 영어 성적: " + englishScore + ", 수학 성적: " + mathScore;
+    }
 }
