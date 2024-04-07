6a5518d296b83a07ca92533374ce86917bbbdb0e
icegosimperson <hooho931@gmail.com>
2024-04-07 17:56:08 +0900

    6. 프로그램 종료 조건 추가

--- Management/Main.java
+++ Management/Main.java
@@ -20,30 +20,42 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("학생 관리 프로그램입니다. ");
-        System.out.println("1번 : 학생 등록, 2번 : 전체 조회, 3번 : 학생 검색, 4번 : 학생 정보 수정, 5번 : 학생 삭제");
 
-        int input = sc.nextInt();
-        switch (input) {
-            case 1:
-                System.out.println("1번: 학생 등록 기능입니다.");
-                Register.registerStudent(sc, students);
-                break;
-            case 2:
-                System.out.println("2번: 전체 조회 기능입니다.");
-                Fetch.fetchStudents(students);
-                break;
-            case 3:
-                System.out.println("3번: 학생 검색 기능입니다.");
-                Search.searchStudent(sc, students);
-                break;
-            case 4:
-                System.out.println("4번: 학생 정보 수정 기능입니다.");
-                Modify.modifyStudent(students, sc);
-                break;
-            case 5:
-                System.out.println("5번: 학생 삭제 기능입니다.");
-                Delete.deleteStudent(students);
-                break;
+        while(true) {
+            System.out.println("1번 : 학생 등록, 2번 : 전체 조회, 3번 : 학생 검색, 4번 : 학생 정보 수정, 5번 : 학생 삭제, 6번 : 프로그램 종료");
+            int input = sc.nextInt();
+
+            switch (input) {
+                case 1:
+                    System.out.println("1번: 학생 등록 기능입니다.");
+                    Register.registerStudent(sc, students);
+                    System.out.println("");
+                    break;
+                case 2:
+                    System.out.println("2번: 전체 조회 기능입니다.");
+                    Fetch.fetchStudents(students);
+                    System.out.println("");
+                    break;
+                case 3:
+                    System.out.println("3번: 학생 검색 기능입니다.");
+                    Search.searchStudent(sc, students);
+                    System.out.println("");
+                    break;
+                case 4:
+                    System.out.println("4번: 학생 정보 수정 기능입니다.");
+                    Modify.modifyStudent(students, sc);
+                    System.out.println("");
+                    break;
+                case 5:
+                    System.out.println("5번: 학생 삭제 기능입니다.");
+                    Delete.deleteStudent(students);
+                    System.out.println("");
+                    break;
+                case 6:
+                    System.out.println("프로그램을 종료합니다.");
+                    return; // 프로그램 종료
+
+            }
         }
     }
 }
