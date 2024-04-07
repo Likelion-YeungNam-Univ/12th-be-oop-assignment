43213280df311d6d993fafe5c412fbdf0963c853
icegosimperson <hooho931@gmail.com>
2024-04-07 17:46:15 +0900

    학생 데이터 추가

--- Management/Main.java
+++ Management/Main.java
@@ -8,6 +8,16 @@ public class Main {
     private static ArrayList<Student> students = new ArrayList<>();
 
     public static void main(String[] args) {
+        // 학생 데이터 추가
+        Student student1 = new Student("22012464", "lion1", 80, 80, 80);
+        students.add(student1);
+
+        Student student2 = new Student("22134265", "lion2", 75, 90, 85);
+        students.add(student2);
+
+        Student student3 = new Student("22227523", "lion3", 95, 85, 95);
+        students.add(student3);
+
         Scanner sc = new Scanner(System.in);
         System.out.println("학생 관리 프로그램입니다. ");
         System.out.println("1번 : 학생 등록, 2번 : 전체 조회, 3번 : 학생 검색, 4번 : 학생 정보 수정, 5번 : 학생 삭제");
