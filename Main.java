d48f7ab2a87aab7dde348794cd327c77f2c5f7db
wlstj <22020306jinseoyeon@gmail.com>
2024-04-07 23:13:38 +0900

    Student, Management class 생성

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("1. 학생 등록    2. 학생 전체 조회     3. 학생 검색    4. 학생 정보 수정     5. 학생 삭제");
+
+        Scanner scn = new Scanner(System.in);
+        int menu = scn.nextInt();
+
+        switch (menu) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+
+        }
+    }
+}
+
+
---
+++ Student.java
@@ -0,0 +1,41 @@
+//학생 클래스
+public class Student {
+
+    private String id;
+    private String name;
+    private String major;
+
+    private int k_score;
+    private int e_score;
+    private int m_score;
+
+    Student() {}
+
+//학생 등록
+    Student(String id, String name, String major, int k_score, int e_score, int m_score){
+        this.id=id;
+        this.name=name;
+        this.major=major;
+        this. k_score= k_score;
+        this. e_score= e_score;
+        this. m_score=m_score;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public int getK_score() {
+        return k_score;
+    }
+    public int getE_score() {return e_score;}
+    public int getM_score() {
+        return m_score;
+    }
+
+
+}
