b9123ed42f8103e790c19fb0ad8a734aa5d0f45c
wlstj <22020306jinseoyeon@gmail.com>
2024-04-08 09:53:01 +0900

    Student class 수정

--- Student.java
+++ Student.java
@@ -9,7 +9,6 @@ public class Student {
 
     Student() {}
 
-//학생 등록
     Student(String id, String name, int k_score, int e_score, int m_score){
         this.id=id;
         this.name=name;
@@ -20,16 +19,31 @@ public class Student {
     public String getId() {
         return id;
     }
+    public void setId(String id){
+        this.id=id;
+    }
     public String getName() {
         return name;
     }
-    public int getK_score() {
+    public int getKscore() {
         return k_score;
     }
-    public int getE_score() {return e_score;}
-    public int getM_score() {
+    public int getEscore() {return e_score;}
+    public int getMscore() {
         return m_score;
     }
 
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setKscore(int k_score){
+        this.k_score=k_score;
+    }
+    public void setEscore(int e_score){
+        this.e_score=e_score;
+    }
+    public void setMscore(int m_score){
+        this.m_score=m_score;
+    }
 
 }
