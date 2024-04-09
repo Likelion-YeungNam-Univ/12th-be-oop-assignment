b98c8d1c9ab248bd516c02da8e069cd0c66ec22d
ParkHa <ugauga19892556@gmail.com>
2024-04-09 17:17:38 +0900

    브랜치 생성

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    String name = null;
+    String number = null;
+    int kor;
+    int eng;
+    int math;
+
+
+    public Student(String name, String number, int kor, int eng, int math) {
+        this.name = name;
+        this.number = number;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+}
+
+
