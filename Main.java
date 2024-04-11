fd88f67c0a00ceee16fce7c8af1508775639ba21
gayoi <117894300+gayoi@users.noreply.github.com>
2024-04-11 19:37:20 +0900

    feat: 프로그램 종료

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("== 학생 관리 프로그램 ==");
 
+        while (true) {
+            System.out.print("입력: ");
 
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("종료")) {
+                break;
+            }
+
+            System.out.printf("입력받은 명령어 : %s\n", cmd);
+
+        }
     }
 }
