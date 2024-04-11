d9b3a0cca5274b7e891571eec369d6367f40d67e
gayoi <117894300+gayoi@users.noreply.github.com>
2024-04-11 20:13:15 +0900

    feat: 성적 등록

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -5,6 +7,7 @@ public class Main {
         System.out.println("== 학생 관리 프로그램 ==");
 
         int lastStudentId = 0;
+        List<Student> students = new ArrayList<>();
 
         while (true) {
             System.out.print("입력: ");
@@ -17,14 +20,30 @@ public class Main {
             }
             else if (cmd.equals("등록")) {
                 System.out.print("학번 : ");
-                String id = scanner.nextLine();
+                String studentId = scanner.nextLine();
 
                 System.out.print("이름 : ");
-                String name = scanner.nextLine();
+                String name = scanner.next();
+
+                System.out.print("국어 성적 : ");
+                int korGr = scanner.nextInt();
+
+                System.out.print("영어 성적 : ");
+                int engGr = scanner.nextInt();
+
+                System.out.print("수학 성적 : ");
+                int matGr = scanner.nextInt();
+
                 lastStudentId++;
+                int id = lastStudentId;
+
+                Student student = new Student(studentId, name, korGr, engGr,matGr);
+                students.add(student);
 
                 System.out.printf("%d번 학생이 등록되었습니다.\n", lastStudentId);
-
+            }
+            else if (cmd.equals("목록")) {
+                System.out.println("총 학생 수 : " + students.size());
             }
         }
     }
--- Student.java
+++ Student.java
@@ -1,14 +1,14 @@
 
 
 public class Student {
-    private String id;
+    private String studentId;
     private String name;
     private int korGr;
     private int engGr;
     private int matGr;
 
-    public Student(String id, String name, int korGr, int engGr, int matGr) {
-        this.id = id;
+    public Student(String studentId, String name, int korGr, int engGr, int matGr) {
+        this.studentId = studentId;
         this.name = name;
         this.korGr = korGr;
         this.engGr = engGr;
