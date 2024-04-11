126fee288af6c418e68a7a9333e484f8090f6f0e
gayoi <117894300+gayoi@users.noreply.github.com>
2024-04-11 19:23:33 +0900

    feat:student class

---
+++ Student.java
@@ -0,0 +1,19 @@
+
+
+public class Student {
+    private int id;
+    private String name;
+    private int korGr;
+    private int engGr;
+    private int matGr;
+
+    public Student(int id, String name, int korGr, int engGr, int matGr) {
+        this.id = id;
+        this.name = name;
+        this.korGr = korGr;
+        this.engGr = engGr;
+        this.matGr = matGr;
+    }
+
+}
+
